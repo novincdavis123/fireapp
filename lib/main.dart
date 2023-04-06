42f3d04600592ad0e536e5fb40ca87784bed8f8c
novincdavis123 <novindavisks@gmail.com>
2023-04-06 20:20:56 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:fireapp/signup.dart';
+import 'package:fireapp/log.dart';
 import 'package:fireapp/welcome.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -12,6 +12,6 @@ void main() async {
     useInheritedMediaQuery: true,
     theme: ThemeData(primarySwatch: Colors.blue),
     debugShowCheckedModeBanner: false,
-    home: user == null ? Log() : Welcome(),
+    home: user == null ? Firelog() : Welcome(),
   ));
 }
